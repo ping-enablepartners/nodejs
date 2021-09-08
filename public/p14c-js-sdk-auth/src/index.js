0340a50dbed36c68cfb5f6ad65bed756f81524d1
ericapplonie <75802029+ericapplonie@users.noreply.github.com>
2021-09-08 09:47:57 -0600

    final?

--- public/p14c-js-sdk-auth/src/index.js
+++
@@ -1,6 +0,0 @@
-export { default as PingOneAuthClient } from "./auth.js";
-export { default as CookiesStorageManager } from "./cookiesStorageManager.js";
-export { default as TokenStorageManager } from "./tokenStorageManager.js";
-export { default as PKCE } from "./pkceManager.js";
-export { default as TokenManager } from "./tokenManager.js";
-export { default as tokenUtils } from "./tokenUtils.js";
--- public/p14c-js-sdk-core/src/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Http } from "./http.js";
-export { default as logger } from "./logger.js";
-export { default as utils } from "./utils.js";
